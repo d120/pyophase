94dae595583c85aeea5009756582e2b95dd3d794
ckleemann <ckleemann@d120.de>
2016-05-29 22:48:06 +0200

    Add email on successfull staff registration #12

--- staff/views.py
+++ staff/views.py
@@ -1,11 +1,12 @@
 from django.views.generic.edit import CreateView
 from django.views.generic import TemplateView, ListView
 from django.urls import reverse_lazy
+from django.core.mail import EmailMessage
 from django.db import IntegrityError
+from django.db.models.query import QuerySet
 from django.template import loader
 from django.template.response import TemplateResponse
 from django.utils.translation import ugettext as _
-
 from ophasebase.models import Ophase
 from staff.models import Settings, GroupCategory, OrgaJob, HelperJob
 from staff.forms import PersonForm
@@ -48,11 +49,70 @@ class StaffAdd(CreateView):
 
     def form_valid(self, form):
         try:
-            return super().form_valid(form)
+            super_return = super().form_valid(form)
         except IntegrityError:
             # this should happen when unique constraints fail
             template = loader.get_template("staff/already_registered.html")
             return TemplateResponse(self.request, template)
+        except:
+            return super_return
+
+        # the enumeration symbol
+        esym = '\n * '
+
+        form_list = []
+
+        for field in form.fields:
+            label = form[field].label
+            # remove html from label
+            label = label.split('<',1)[0].strip()
+
+            content = form.cleaned_data[field]
+            #Remove all fields that are not set
+            if content:
+                #format Many to Many and foreign key
+                if isinstance(content, QuerySet):
+                    content = esym + esym.join(str(c) for c in content)
+                #format True as normal language
+                if isinstance(content, bool):
+                    content = _('Ja')
+
+                form_list.append('{}: {}'.format(label, content))
+
+        form_content = '\n'.join(form_list)
+
+        values = {'ophasen_title': str(Ophase.current()),
+                 'user_prename': form.cleaned_data['prename'],
+                 'user_name':  form.cleaned_data['name'],
+                 'user_email': form.cleaned_data['email'],
+                 'email_changedata': 'ophase-leitung@d120.de',
+                 'form_content': form_content,
+                 }
+        
+        email = EmailMessage()
+        email.subject = _('{ophasen_title} Registrierung').format(**values)
+        email.to = ['{user_prename} {user_name} <{user_email}>'.format(**values)]
+
+        email.body = _("""Hallo {user_prename},
+
+vielen Dank dass du mithelfen möchtest. Deine Daten wurden erfolgreich 
+gespeichert. Die Ophasenleitung wird sich in geraumer Zeit bei dir melden, 
+sobald erste organisatorische Dinge oder Termine anstehen. Dies kann unter 
+Umständen noch ein paar Wochen dauern.
+
+Falls sich in der Zwischenzeit Daten von dir ändern sende uns doch bitte eine 
+E-Mail an {email_changedata}.
+
+Hier nochmal die von dir eingetragenen Daten:
+
+{form_content}
+
+Viele Grüße,
+Die Ophasen-Leitung""").format(**values)
+        
+        email.send()
+
+        return super_return
 
 class StaffAddSuccess(TemplateView):
     template_name = 'staff/success.html'
