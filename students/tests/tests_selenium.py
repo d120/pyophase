1f492d3e4c3508f7296533dd14cd3aa86dede5ff
ckleemann <ckleemann@d120.de>
2016-06-04 16:27:53 +0200

    remove unused functions

--- students/tests/tests_selenium.py
+++ students/tests/tests_selenium.py
@@ -108,24 +108,3 @@ class StudentSeleniumTests(StaticLiveServerTestCase):
         self.assertTrue(driver.find_element_by_id("id_email").get_attribute("required"))
         self.assertEqual("", driver.find_element_by_id("id_email").get_attribute("value"))
         self.assertTrue(driver.find_element_by_id("id_email").is_displayed())
-        
-    def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
