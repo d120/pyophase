e06f8ad30caf9470243d117ab3bd13d31cdccce5
exploide <me@exploide.net>
2016-07-06 15:00:30 +0200

    removed deprecated app_name parameter from url includes

--- pyophase/urls.py
+++ pyophase/urls.py
@@ -6,11 +6,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^', include('ophasebase.urls', namespace='ophasebase', app_name='ophasebase')),
-    url(r'^mitmachen/', include('staff.urls', namespace='staff', app_name='staff')),
-    url(r'^teilnehmer/', include('students.urls', namespace='students', app_name='students')),
-    url(r'^klausur/', include('exam.urls', namespace='exam', app_name='exam')),
-    url(r'^dashboard/', include('dashboard.urls', namespace='dashboard', app_name='dashboard')),
+    url(r'^', include('ophasebase.urls', namespace='ophasebase')),
+    url(r'^mitmachen/', include('staff.urls', namespace='staff')),
+    url(r'^teilnehmer/', include('students.urls', namespace='students')),
+    url(r'^klausur/', include('exam.urls', namespace='exam')),
+    url(r'^dashboard/', include('dashboard.urls', namespace='dashboard')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', django.contrib.auth.views.login, {'template_name': 'admin/login.html'}, name='login'),
     url(r'^accounts/logout/$', django.contrib.auth.views.logout, {'next_page': reverse_lazy('ophasebase:welcome')}, name='logout'),
