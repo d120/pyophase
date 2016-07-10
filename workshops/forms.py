a44d1f598af221efcf3274ac1d3609bd3acaf644
exploide <me@exploide.net>
2016-07-10 18:11:07 +0200

    moved model dependant widget attr to __init__ method to make migrations functional again

--- workshops/forms.py
+++ workshops/forms.py
@@ -6,7 +6,8 @@ class WorkshopSubmissionForm(forms.ModelForm):
     class Meta:
         model = Workshop
         exclude = ['ophase']
-        widgets = {
-            'how_often': forms.NumberInput(attrs={'min': '1', 'max': str(WorkshopSlot.get_current().count()), 'step': '1', 'value': '1'}),
-            'max_participants': forms.NumberInput(attrs={'value': '0'}),
-        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['how_often'].widget = forms.NumberInput(attrs={'min': '1', 'max': str(WorkshopSlot.get_current().count()), 'step': '1', 'value': '1'})
+        self.fields['max_participants'].widget = forms.NumberInput(attrs={'value': '0'})
