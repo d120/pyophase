d2e1f279b7645b3b1f837122e0d6081cce9a840a
exploide <me@exploide.net>
2016-07-11 15:16:37 +0200

    added workshop widget

--- dashboard/dashboard_widgets.py
+++ dashboard/dashboard_widgets.py
@@ -2,6 +2,7 @@ from exam.dashboard_widgets import ExamWidget
 from ophasebase.dashboard_components import CountdownWidget
 from staff.dashboard_widgets import StaffCountWidget
 from students.dashboard_widgets import StudentCountWidget
+from workshops.dashboard_widgets import WorkshopCountWidget
 
 
 class DashboardWidgets():
@@ -9,5 +10,6 @@ class DashboardWidgets():
         CountdownWidget(),
         StaffCountWidget(),
         StudentCountWidget(),
+        WorkshopCountWidget(),
         ExamWidget()
     ]
---
+++ workshops/dashboard_widgets.py
@@ -0,0 +1,20 @@
+from django.utils.translation import ugettext_lazy as _
+
+from dashboard.components import TemplateWidgetComponent
+from ophasebase.models import Ophase
+from workshops.models import Workshop
+
+class WorkshopCountWidget(TemplateWidgetComponent):
+    name = _('Workshops')
+    template_name = "workshops/dashboard/widget_workshops.html"
+    status = 'info'
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        current_ophase = Ophase.current()
+        if current_ophase is not None:
+            workshops = Workshop.get_current()
+            context['num_workshops'] = workshops.count()
+            context['filled_slots'] = sum(w.how_often for w in workshops)
+            context['num_workshop_tutors'] = len(set(w.tutor_mail for w in workshops))
+        return context
