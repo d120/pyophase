b12775b129689e86dd0da59a4ba40abfeddb9cb2
Johannes Lauinger <johannes@lauinger-it.de>
2016-08-11 17:41:40 +0200

    Creates four subpages for website and refactors base css into general file

--- website/urls.py
+++ website/urls.py
@@ -5,4 +5,8 @@ from website import views
 app_name = 'website'
 urlpatterns = [
     url(r'^$', views.HomepageView.as_view(), name='homepage'),
+    url(r'^bachelor/$', views.BachelorView.as_view(), name='bachelor'),
+    url(r'^master-de/$', views.MasterDeView.as_view(), name='master-de'),
+    url(r'^master-dss/$', views.MasterDssView.as_view(), name='master-dss'),
+    url(r'^helfen/$', views.HelfenView.as_view(), name='helfen'),
 ]
--- website/views.py
+++ website/views.py
@@ -9,3 +9,39 @@ class HomepageView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['current_ophase'] = Ophase.current()
         return context
+
+
+class BachelorView(TemplateView):
+    template_name = "website/bachelor.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_ophase'] = Ophase.current()
+        return context
+
+
+class MasterDeView(TemplateView):
+    template_name = "website/master-de.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_ophase'] = Ophase.current()
+        return context
+
+
+class MasterDssView(TemplateView):
+    template_name = "website/master-dss.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_ophase'] = Ophase.current()
+        return context
+
+
+class HelfenView(TemplateView):
+    template_name = "website/helfen.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_ophase'] = Ophase.current()
+        return context
