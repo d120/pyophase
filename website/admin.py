9d9ebf36a9643c778488410b1c80e6d24e88d376
Johannes Lauinger <johannes@lauinger-it.de>
2016-08-12 18:50:47 +0200

    adds settings model to website app, to specify dynamic content on pages

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from website.models import Settings
 
-# Register your models here.
+
+@admin.register(Settings)
+class SettingsAdmin(admin.ModelAdmin):
+    list_display = ['helpdesk_phone_number']
--- website/models.py
+++ website/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Settings(models.Model):
+    """Configuration for Website App."""
+    class Meta:
+        verbose_name = _("Einstellungen")
+        verbose_name_plural = _("Einstellungen")
+
+    helpdesk_phone_number = models.CharField(max_length=50)
+
+    @staticmethod
+    def get_name():
+        return '%s' % _("Website Einstellungen")
+
+    def __str__(self):
+        return self.get_name()
+
+    def clean(self, *args, **kwargs):
+        super().clean()
+        if Settings.objects.count() > 0 and self.id != Settings.objects.get().id:
+            raise ValidationError(_("Es ist nur sinnvoll und möglich eine Instanz des Einstellungsobjekts anzulegen."))
+
+    @staticmethod
+    def instance():
+        try:
+            return Settings.objects.get()
+        except Settings.DoesNotExist:
+            return None
