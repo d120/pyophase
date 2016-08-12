97c249fe158d6ff8d094c05f18f23ddb50b11fcf
Johannes Lauinger <johannes@lauinger-it.de>
2016-08-12 18:40:06 +0200

    show registration blocks on homepage conditionally based on settings objects

--- website/views.py
+++ website/views.py
@@ -1,5 +1,8 @@
 from django.views.generic import TemplateView
 from ophasebase.models import Ophase
+from students.models import Settings as StudentsSettings
+from staff.models import Settings as StaffSettings
+from workshops.models import Settings as WorkshopSettings
 
 
 class HomepageView(TemplateView):
@@ -7,7 +10,23 @@ class HomepageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['current_ophase'] = Ophase.current()
+        students_settings = StudentsSettings.instance()
+        staff_settings = StaffSettings.instance()
+        workshop_settings = WorkshopSettings.instance()
+        current_ophase = Ophase.current()
+        context['current_ophase'] = current_ophase
+
+        if current_ophase is not None and students_settings is not None:
+            context['student_registration_enabled'] = students_settings.student_registration_enabled
+        else:
+            context['student_registration_enabled'] = False
+
+        context['any_staff_registration_enabled'] = False
+        if current_ophase is not None and staff_settings is not None:
+            context['any_staff_registration_enabled'] = staff_settings.tutor_registration_enabled or staff_settings.orga_registration_enabled or staff_settings.helper_registration_enabled
+        if current_ophase is not None and workshop_settings is not None:
+            context['any_staff_registration_enabled'] = context['any_staff_registration_enabled'] or workshop_settings.workshop_submission_enabled
+
         return context
 
 
