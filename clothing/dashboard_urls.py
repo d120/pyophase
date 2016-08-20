e6f651fe2f5c244f51b9d7c8b7e801a666fb392b
Benjamin Haettasch <benjamin.haettasch@googlemail.com>
2016-08-20 22:58:58 +0200

    Clothing: Dashboard view to show which persons already ordered their free piece of clothing

--- clothing/dashboard_urls.py
+++ clothing/dashboard_urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from clothing.dashboard_views import OrderOverView
+from clothing.dashboard_views import OrderOverView, FreeClothingView
 
 app_name = 'clothing'
 urlpatterns = [
-    url(r'^orders$', OrderOverView.as_view(), name='order_overview'),
+    url(r'^orders/$', OrderOverView.as_view(), name='order_overview'),
+    url(r'^orders/free/$', FreeClothingView.as_view(), name='order_free'),
 ]
--- clothing/dashboard_views.py
+++ clothing/dashboard_views.py
@@ -1,9 +1,10 @@
+from django.db import models
 from django.utils.translation import ugettext as _
 from django.views.generic import ListView
 
 from clothing.models import Order
 from dashboard.components import DashboardAppMixin
-from ophasebase.models import Ophase
+from staff.models import Person
 
 
 class ClothingAppMixin(DashboardAppMixin):
@@ -15,6 +16,7 @@ class ClothingAppMixin(DashboardAppMixin):
     def sidebar_links(self):
         return [
             (_('Übersicht'), self.prefix_reverse_lazy('order_overview')),
+            (_('Grundbestellung'), self.prefix_reverse_lazy('order_free')),
         ]
 
 
@@ -25,3 +27,23 @@ class OrderOverView(ClothingAppMixin, ListView):
 
     def get_queryset(self):
         return Order.get_current()
+
+
+class FreeClothingView(ClothingAppMixin, ListView):
+    model = Person
+    template_name = "clothing/dashboard/orders_person_free.html"
+    context_object_name = "persons"
+
+    def get_queryset(self):
+        return Person.get_current()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['persons'] = context['persons'].annotate(
+            num_free_orders=models.Sum(models.Case(
+                models.When(order__additional=False, then=1),
+                default=0,
+                output_field=models.IntegerField()
+            ))
+        )
+        return context
