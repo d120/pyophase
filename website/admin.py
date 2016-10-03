449ff48975841d6b975f224727c1a674ec21817b
Johannes Lauinger <johannes@lauinger-it.de>
2016-10-03 22:29:10 +0200

    include new OInforz model in admin area

--- website/admin.py
+++ website/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 
-from .models import Schedule, Settings
+from .models import Schedule, Settings, OInforz
 
 
 @admin.register(Settings)
 class SettingsAdmin(admin.ModelAdmin):
     list_display = ['helpdesk_phone_number', 'vorkurs_start_date', 'vorkurs_end_date']
 
-@admin.register(Schedule)
-class SettingsAdmin(admin.ModelAdmin):
-    list_display = ['degree','stand']
 
+@admin.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ['degree', 'stand']
+
+
+@admin.register(OInforz)
+class OInforzAdmin(admin.ModelAdmin):
+    list_display = ['degree', 'stand']
