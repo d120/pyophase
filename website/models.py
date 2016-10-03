8855f113924d8d612ad0f33e813103c492d3c18a
Johannes Lauinger <johannes@lauinger-it.de>
2016-10-03 22:28:54 +0200

    adds OInforz model similar to the existing one for schedules

--- website/models.py
+++ website/models.py
@@ -9,7 +9,6 @@ from django.template.defaultfilters import date as _date
 from django.utils.translation import ugettext_lazy as _
 
 
-
 class Settings(models.Model):
     """Configuration for Website App."""
     class Meta:
@@ -44,11 +43,13 @@ class Settings(models.Model):
         except Settings.DoesNotExist:
             return None
 
+
 class OverwriteStorage(FileSystemStorage):
     """Delete the old file so that the name is available"""
     def get_available_name(self, name, max_length):
         self.delete(name)
         return super().get_available_name(name, max_length)
+
 
 class Schedule(models.Model):
     """A Schedule for a Degree during the Ophase"""
@@ -79,7 +80,42 @@ class Schedule(models.Model):
     def __str__(self):
         return '{} {}'.format(self.get_degree_display(), _date(self.stand, 'SHORT_DATE_FORMAT'))
 
+
 # Register a signal receiver so the image is deleted when the model is deleted
 @receiver(pre_delete, sender=Schedule)
 def schedule_delete(sender, instance, **kwargs):
     instance.image.delete(False)
+
+
+class OInforz(models.Model):
+    """An OInforz PDF booklet version"""
+    class Meta:
+        verbose_name = _("OInforz")
+        verbose_name_plural = _("OInforze")
+
+    DEGREE_CHOICES = (
+        ('BSC', _('Bachelor')),
+        ('MSC', _('Master Deutsch')),
+        ('DSS', _('Distributed Software Systems')),
+    )
+
+    def fixedname_upload_to(instance, filename):
+        """returns the path and name where the image should upload to"""
+        path = 'website/oinforz/'
+        name = instance.degree.lower()
+        x, file_extension = os.path.splitext(filename)
+        return '{}{}{}'.format(path, name, file_extension)
+
+    degree = models.CharField(max_length=3, choices=DEGREE_CHOICES, verbose_name=_('Abschluss'), unique=True)
+    file = models.FileField(verbose_name=_('OInforz PDF'),
+                upload_to=fixedname_upload_to, storage=OverwriteStorage())
+    stand = models.DateField(verbose_name=_('Stand des OInforzes'))
+
+    def __str__(self):
+        return '{} {}'.format(self.get_degree_display(), _date(self.stand, 'SHORT_DATE_FORMAT'))
+
+
+# Register a signal receiver so the document is deleted when the model is deleted
+@receiver(pre_delete, sender=OInforz)
+def oinforz_delete(sender, instance, **kwargs):
+    instance.file.delete(False)
