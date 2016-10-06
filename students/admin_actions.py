000e945726c6dad6a8c17beaf3d007cfdf6332a1
Johannes Lauinger <johannes@lauinger-it.de>
2016-10-06 19:56:42 +0200

    use ophase schein.sty like in good old times

--- students/admin_actions.py
+++ students/admin_actions.py
@@ -1,12 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import get_template
 
 from ophasebase.helper import LaTeX
 
 
 def generate_part_cert(modeladmin, request, queryset):
     """ Generates a PDF file with exam certificates for selected students and sends it to the browser """
-    (pdf, pdflatex_output) = LaTeX.render(queryset, 'students/reports/exam-report.tex', ['scheine.sty', 'kif_logo.png'], 'students')
+    (pdf, pdflatex_output) = LaTeX.render(queryset, 'students/reports/exam-report.tex', ['scheine.sty', 'scheinVorlagen.sty', 'OPhasenWesen_2015.png'], 'students')
     if pdf == None:
         return render(request, "students/reports/rendering-error.html", { "content": pdflatex_output[0].decode("utf-8") })
     r = HttpResponse(content_type='application/pdf')
