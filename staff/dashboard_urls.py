4e64101409de4a76d0bd36932ad62fb42972e53c
ckleemann <ckleemann@users.noreply.github.com>
2016-10-12 16:15:03 +0200

    Add slashs at the end of URLs

--- staff/dashboard_urls.py
+++ staff/dashboard_urls.py
@@ -9,9 +9,9 @@ from .dashboard_views import (
 
 app_name = 'staff'
 urlpatterns = [
-    url(r'^group_mass_create$', GroupMassCreateView.as_view(), name='group_mass_create'),
-    url(r'^tutor_pairing$', TutorPairingView.as_view(), name='tutor_pairing'),
-    url(r'^tutor_pairing_success$', TutorPairingSuccess.as_view(), name='tutor_pairing_success'),
+    url(r'^group_mass_create/$', GroupMassCreateView.as_view(), name='group_mass_create'),
+    url(r'^tutor_pairing/$', TutorPairingView.as_view(), name='tutor_pairing'),
+    url(r'^tutor_pairing_success/$', TutorPairingSuccess.as_view(), name='tutor_pairing_success'),
 
     url(r'^event/$', AttendanceEventIndexView.as_view(), name='event_index'),
     url(r'^event/(?P<pk>[0-9]+)/$', AttendanceEventDetailView.as_view(), name='event_detail'),
