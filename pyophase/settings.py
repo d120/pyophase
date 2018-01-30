5824c3672fc38d58af335bfa2b6aaa99f8973915
ckleemann <ckleemann@users.noreply.github.com>
2018-01-30 15:26:46 +0100

    remove admin doc fixes #185

--- pyophase/settings.py
+++ pyophase/settings.py
@@ -35,7 +35,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
     'django.contrib.admin',
-    'django.contrib.admindocs',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
