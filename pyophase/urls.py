2e238caab8b17f34ff64edf54526b02203ba8c26
ckleemann <ckleemann@users.noreply.github.com>
2019-01-19 15:08:17 +0100

    Updated admin views to new style

--- pyophase/urls.py
+++ pyophase/urls.py
@@ -1,4 +1,4 @@
-import django.contrib.auth.views
+from django.contrib.auth.views import LoginView, LogoutView
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
@@ -18,8 +18,8 @@ urlpatterns = [
     url(r'^dashboard/', include('dashboard.urls', namespace='dashboard')),
     url(r'^clothing/', include('clothing.urls', namespace='clothing')),
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/$', django.contrib.auth.views.login, {'template_name': 'admin/login.html'}, name='login'),
-    url(r'^accounts/logout/$', django.contrib.auth.views.logout, {'next_page': reverse_lazy('website:homepage')}, name='logout'),
+    url(r'^accounts/login/$', LoginView.as_view(), name='login'),
+    url(r'^accounts/logout/$', LogoutView.as_view(next_page=reverse_lazy('website:homepage')), name='logout'),
     url(r'^i18n/', include('django.conf.urls.i18n')),
 ]
 
