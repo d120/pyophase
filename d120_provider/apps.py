54cdea82dc266a8bffc25e56e7dc0ffadebe9bda
ckleemann <ckleemann@users.noreply.github.com>
2019-07-11 23:33:03 +0200

    addet group sync logic

--- d120_provider/apps.py
+++ d120_provider/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class D120ProviderConfig(AppConfig):
     name = 'd120_provider'
+
+    def ready(self):
+        # noinspection PyUnresolvedReferences
+        from .signals import sig_sync_user_to_groups
