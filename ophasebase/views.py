916d1f91acfc5fd1fcd300c87b4264e167666a19
ckleemann <ckleemann@users.noreply.github.com>
2019-08-23 15:45:14 +0200

    rewrite next url

--- ophasebase/views.py
+++ ophasebase/views.py
@@ -2,3 +2,10 @@ from django.views.generic import TemplateView
 
 class LoginSelectView(TemplateView):
     template_name = "ophasebase/login-select.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()  #
+        next_url = self.request.GET.get('next', None)
+        if next_url is not None:
+            context['next_url'] = "?next={}".format(next_url)
+        return context
