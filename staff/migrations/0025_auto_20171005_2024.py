5c9646d00407b79010110c1294c86472342d78f6
ckleemann <ckleemann@users.noreply.github.com>
2021-02-07 23:52:34 +0100

    Rewrite migrations so pytuid can be zeroed #370

--- staff/migrations/0025_auto_20171005_2024.py
+++ staff/migrations/0025_auto_20171005_2024.py
@@ -3,7 +3,6 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
@@ -16,6 +15,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='person',
             name='tuid',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='pyTUID.TUIDUser', verbose_name='TUID User'),
+            field=models.BooleanField(null=True, default=True, verbose_name='TUID User'),
         ),
     ]
