a9202e5a5a713970c1ef9d9d654cb76387b453b7
ckleemann <ckleemann@users.noreply.github.com>
2021-02-07 18:49:37 +0100

    init version of github actions

--- pyophase/settings_github_actions.py
+++ pyophase/settings_github_actions.py
@@ -1,12 +1,10 @@
-from .settings import *
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'HOST': 'localhost',
         'NAME': 'pyophase',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'pyophase',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
