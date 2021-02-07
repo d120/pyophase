2030db96e786b8d641d1b1f30d4009c5a9054f27
ckleemann <ckleemann@users.noreply.github.com>
2021-02-07 22:45:14 +0100

    remove d120_provider

--- d120_provider/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'd120_provider.apps.D120ProviderConfig'
--- d120_provider/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import GroupSync
-
-
-@admin.register(GroupSync)
-class GroupSyncAdmin(admin.ModelAdmin):
-    list_display = ('external_group', 'django_group')
