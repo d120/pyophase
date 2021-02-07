f38ad74b85bd6aeef3cb6e04aa5a9200347e2ff2
ckleemann <ckleemann@users.noreply.github.com>
2021-02-07 19:01:50 +0100

    set port to avoid socket connection

--- pyophase/settings_github_actions.py
+++ pyophase/settings_github_actions.py
@@ -3,7 +3,8 @@ from .settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
         'NAME': 'pyophase',
         'USER': 'root',
         'PASSWORD': 'pyophase',
