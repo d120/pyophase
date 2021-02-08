aeb90ef4367eaf33e2902caca50e2659ef88b445
Heiko Carrasco <4395770+miterion@users.noreply.github.com>
2021-02-08 12:55:56 +0100

    Revert "Rewrite migrations so pytuid can be zeroed #370"
    This reverts commit 5c9646d00407b79010110c1294c86472342d78f6.

--- staff/migrations/0019_auto_20170526_1421.py
+++ staff/migrations/0019_auto_20170526_1421.py
@@ -3,11 +3,13 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('pyTUID', '0003_auto_20170514_1146'),
         ('ophasebase', '0007_auto_20170215_1416'),
         ('staff', '0018_person_tutor_experience'),
     ]
@@ -16,10 +18,10 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='person',
             name='tuid',
-            field=models.BooleanField(null=True, default=True, verbose_name='TUID User'),
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='pyTUID.TUIDUser', verbose_name='TUID User'),
         ),
         migrations.AlterUniqueTogether(
             name='person',
-            unique_together=set([('ophase', 'email'), ]),
+            unique_together=set([('ophase', 'email'), ('ophase', 'tuid')]),
         ),
     ]
